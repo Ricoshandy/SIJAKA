e09eaa517768b756d0fdb9583d25d921fb9f7c6d
Ilham Sidik <ilhamsidikc@gmail.com>
2025-03-16 10:14:04 +0700

    add new configuration routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,35 @@
 <?php
 
+use App\Http\Middleware\ComiteMiddleware;
+use App\Http\Middleware\DosenMiddleware;
+use App\Http\Middleware\GuestMiddleware;
+use App\Http\Middleware\KepegawaianMiddleware;
+use App\Http\Middleware\SenatMiddleware;
+use App\Http\Middleware\SuperAdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        using: function () {
+            Route::middleware('web')
+                ->group(glob(base_path('routes/web/*.php')));
+            Route::middleware('web')
+                ->group(glob(base_path('routes/web.php')));
+        },
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'superadmin' => SuperAdminMiddleware::class,
+            'dosen' => DosenMiddleware::class,
+            'kepegawaian' => KepegawaianMiddleware::class,
+            'comite' => ComiteMiddleware::class,
+            'senat' => SenatMiddleware::class,
+            'guest' => GuestMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
