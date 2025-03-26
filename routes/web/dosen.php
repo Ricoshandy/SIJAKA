cf3b158afb36a1505e3687ac870bdc7c6800ea66
Ilham Sidik <ilhamsidikc@gmail.com>
2025-03-26 08:52:41 +0700

    update routes

--- routes/web/dosen.php
+++ routes/web/dosen.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FormPengajuanController;
+use App\Http\Controllers\PengajuanController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'dosen'])->prefix('dosen')->group(function(){
@@ -8,6 +10,19 @@ Route::middleware(['auth', 'dosen'])->prefix('dosen')->group(function(){
     // Route Untuk Mengarahkan User dengan Role Dosen ke Dashboard Dosen
     Route::controller(DashboardController::class)->group(function(){
         Route::get('dashboard', 'dosen_dashboard')->name('dosen_dashboard');
+    });
+
+    Route::controller(FormPengajuanController::class)->group(function(){
+        Route::get('pengajuan/form', 'pengajuan_form')->name('pengajuan.form');
+        Route::get('pengajuan/form/{id}', 'form')->name('form');
+        Route::get('pengajuan/list', 'pengajuan_list')->name('pengajuan.list');
+    });
+    
+    Route::controller(PengajuanController::class)->group(function(){
+        Route::post('pengajuan/form/{id}/submit', 'submit')->name('pengajuan.submit');
+        Route::get('pengajuan/view/{id}', 'pengajuan_view')->name('pengajuan.view');
+        Route::get('pengajuan/edit/{id}', 'pengajuan_edit')->name('pengajuan.edit');
+        Route::post('pengajuan/edit/{id}/submit', 'pengajuan_edit_submit')->name('pengajuan.edit.submit');
     });
     
 });
